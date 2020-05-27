7d672b246f255359ee22492c1c2dae4451e9a8f0
Rodney Dyer <rjdyer@vcu.edu>
2020-05-26 22:58:46 -0400

    adding all the posts from the personal blog

--- R/FixFiles.R
+++ R/FixFiles.R
@@ -1,15 +1,15 @@
-files <- list.files("./content/publication/", recursive = TRUE, full.names = TRUE, pattern = "*.md")
+files <- list.files("./content/post/", recursive = TRUE, full.names = TRUE, pattern = "*.md")
 
-
-for( file in files ) {
-  con <- file( file )
-  lines <- readLines( con )
-
-  cat(file)
-
-  ln <- grep("^publication_types",lines)
-
-  print(lines[ln])
-
-  close( con )
-}
+#
+# for( file in files ) {
+#   con <- file( file )
+#   lines <- readLines( con )
+#
+#   cat(file, " ")
+#
+#   ln <- grep("focal",lines)
+#
+#   print(lines[ln])
+#
+#   close( con )
+# }
